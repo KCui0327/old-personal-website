37b5bcdae69b3979fe89e86fb5e9f1980c89eb75
TheKCui0327 <cuikenny9@gmail.com>
2023-01-07 19:08:42 -0800

    edited font

--- my-app/pages/components/Skills.js
+++ my-app/pages/components/Skills.js
@@ -20,7 +20,7 @@ export default function Skills() {
   return (
     <div className="p-10">
         <h3 className="text-3xl py-1">Technical Skills</h3>
-        <h4 className="text-2xl py-3 text-gray-800">Programming Languages</h4>
+        <h4 className="text-xl py-3 text-gray-800">Programming Languages</h4>
           <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
             <ImageColumns description="Python" image={Python}/>
             <ImageColumns description="C++" image={CPP}/>
@@ -31,7 +31,7 @@ export default function Skills() {
             <ImageColumns description="HTML5" image={html}/>
             <ImageColumns description="CSS3" image={css}/>
           </div>
-        <h4 className="text-2xl py-3 text-gray-800">Frameworks / Libraries</h4>
+        <h4 className="text-xl py-3 text-gray-800">Frameworks / Libraries</h4>
           <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
             <ImageColumns description="Git" image={git}/>
             <ImageColumns description="TensorFlow" image={Tensorflow}/>
